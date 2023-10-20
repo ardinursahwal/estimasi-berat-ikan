e64e1c7490707fb498492a3f7dd0d34d31e57dcd
ardinursahwal <148542995+ardinursahwal@users.noreply.github.com>
2023-10-20 17:22:03 +0700

    Update estimasi-berat-ikan.py

--- estimasi-berat-ikan.py
+++ estimasi-berat-ikan.py
@@ -1,1 +1,25 @@
+import pickle
+import streamlit as st
+from sklearn.preprocessing import LabelEncoder
 
+model = pickle.load(open('estimasi_ikan.sav', 'rb'))
+
+st.title('Estimasi Berat Ikan Dalam gram')
+
+Species2 = st.selectbox('Pilih Spesies', ['Perch', 'Bream', 'Roach', 'Pike', 'Smelt', 'Parkki', 'Whitefish'])
+Length1 = st.number_input('Input Panjang vertikal dalam cm')
+Length2 = st.number_input('Input Panjang diagonal dalam cm')
+Length3 = st.number_input('Input Panjang silang dalam cm')
+Height = st.number_input('Input Tinggi dalam cm')
+Width = st.number_input('Input Lebar diagonal dalam cm')
+
+label_encoder_spec = LabelEncoder()
+label_encoder_spec.fit(['Perch', 'Bream', 'Roach', 'Pike', 'Smelt', 'Parkki', 'Whitefish'])
+
+predict = ''
+
+if st.button('Estimasi Berat Ikan'):
+    predict = model.predict(
+        [[Species2,Length1,Length2,Length3,Height,Width]]
+    )
+    st.write ('Estimasi Berat Ikan Dalam gram : ', predict)
