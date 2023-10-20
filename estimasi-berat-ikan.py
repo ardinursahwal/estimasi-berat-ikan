5d5b31d006659b609b45b579567888d0fb3ed4cb
ardinursahwal <148542995+ardinursahwal@users.noreply.github.com>
2023-10-20 17:30:15 +0700

    Update estimasi-berat-ikan.py

--- estimasi-berat-ikan.py
+++ estimasi-berat-ikan.py
@@ -6,7 +6,7 @@ model = pickle.load(open('estimasi_ikan.sav', 'rb'))
 
 st.title('Estimasi Berat Ikan Dalam gram')
 
-Species2 = st.selectbox('Pilih Spesies', ['Perch', 'Bream', 'Roach', 'Pike', 'Smelt', 'Parkki', 'Whitefish'])
+Species = st.selectbox('Pilih Spesies', ['Perch', 'Bream', 'Roach', 'Pike', 'Smelt', 'Parkki', 'Whitefish'])
 Length1 = st.number_input('Input Panjang vertikal dalam cm')
 Length2 = st.number_input('Input Panjang diagonal dalam cm')
 Length3 = st.number_input('Input Panjang silang dalam cm')
@@ -15,11 +15,12 @@ Width = st.number_input('Input Lebar diagonal dalam cm')
 
 LabelEncoder = label_encoder_spec()
 label_encoder_spec.fit(['Perch', 'Bream', 'Roach', 'Pike', 'Smelt', 'Parkki', 'Whitefish'])
+spec_encoded = label_encoder_sex.transform([sex2])[0]
 
 predict = ''
 
 if st.button('Estimasi Berat Ikan'):
     predict = model.predict(
-        [[Species2,Length1,Length2,Length3,Height,Width]]
+        [[spec_encoded,Length1,Length2,Length3,Height,Width]]
     )
     st.write ('Estimasi Berat Ikan Dalam gram : ', predict)
